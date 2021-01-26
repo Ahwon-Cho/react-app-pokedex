181caa525c1d9380d34d05e0f3041eea17616588
Ahwon Cho <awony82@gmail.com>
2021-01-25 20:20:27 -0800

    chaged Pokecard.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import './App.css';
 import Pokedex from './Pokedex'
+import Pokecard from './Pokecard'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Pokedex />
+        <Pokecard 
+        name = "Charmender"
+        id = {4}
+        type = "fire"
+        exp = {6}
+        />
       </div>
     )
   }
