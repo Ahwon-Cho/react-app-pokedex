61751a7dd2f7cac0180b90da65f5e06cfd3e3b85
Ahwon Cho <awony82@gmail.com>
2021-01-26 21:32:17 -0800

    changed css files and the image link

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import './Pokecard.css'
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+// const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/' ;
+
+let padToThree = (number) => (number <= 999 ? `00`.slice(-3))
 
 class Pokecard extends Component {
     render() {
         let imgSrc = ` ${POKE_API}${this.props.id}.png `;
         return(
             <div className="Pokecard">
-                <h2>{this.props.name}</h2>
+                <h2 className="Pokecard-title">{this.props.name}</h2>
                 <img src={imgSrc} alt={this.props.name} />
-                <div>Type: {this.props.type}</div>
-                <div>EXP: {this.props.exp}</div>
+                <div className="Pokecard-data">Type: {this.props.type}</div>
+                <div className="Pokecard-data">EXP: {this.props.exp}</div>
             </div>
         )
     }
